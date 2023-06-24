dfc470ee786b3c7e49ece87e12043d6a28268953
karthixxvx <karthixxvx8@outlook.com>
2023-06-24 18:19:47 -0400

    saved second chg

---
+++ notebooks/00_readme.txt
@@ -0,0 +1,5 @@
+
+https://www.youtube.com/watch?v=yjjE-MJD5TI
+
+conda activate scu
+
--- notebooks/10_test2.py
+++ notebooks/10_test2.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
